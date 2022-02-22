7d17e912b24715bb0a68185f844aa7f6f758ab43
Vandam Dinh <vandam@eduroam-3-56.nomadic.bris.ac.uk>
2022-02-22 15:18:12 +0000

    Updated Graph Generator to work with MDM1

--- Graph_Generator.py
+++ Graph_Generator.py
@@ -8,19 +8,19 @@ import matplotlib.pyplot as plt
 import networkx as nx
 
 # INPUT - Matrix as a List
-Matrix = [(0.09518089791960394, 'BD'), (0.1277378312985179, 'AB'), (0.1419332372816624, 'CD'), 
-          (0.533477090542755, 'AD'), (0.6790313116299046, 'AC'), (0.7306722553371253, 'BC')]
 
-N = int(len(Matrix))
-
-WeightList = [str(item[0])[:6] for item in Matrix]
-FromEdgeList = [item[1][0] for item in Matrix]
-ToEdgeList = [item[1][1] for item in Matrix]
-EdgeList = list(zip(FromEdgeList[::1],ToEdgeList[::1]))                     # Pairs the edges together e.g. [('B', 'D'), ('A', 'B'),...]
-WeightDict = {EdgeList[i]: WeightList[i] for i in range(len(EdgeList))}     # Dictionary pairing Edgelist with Weight // Needed for labelling edges
-
-def GenerateGraph(N):
+def GenerateGraph(Matrix):
+    
+    N = int(len(Matrix))
+    
+    WeightList = [str(item[0])[:6] for item in Matrix]
+    FromEdgeList = [item[1][0] for item in Matrix]
+    ToEdgeList = [item[1][1] for item in Matrix]
+    EdgeList = list(zip(FromEdgeList[::1],ToEdgeList[::1]))                     # Pairs the edges together e.g. [('B', 'D'), ('A', 'B'),...]
+    WeightDict = {EdgeList[i]: WeightList[i] for i in range(len(EdgeList))}     # Dictionary pairing Edgelist with Weight // Needed for labelling edges
+    
     G = nx.Graph()
+    
     for i in range(N):
         # Adding Edges of all the Nodes to a list
         G.add_edge(str(Matrix[i][1][0]), str(Matrix[i][1][1]), weight=int(Matrix[i][0]))
@@ -51,4 +51,4 @@ def GenerateGraph(N):
     # Lets see this beauty!
     plt.show()
 
-GenerateGraph(N)
+#GenerateGraph(Matrix)
